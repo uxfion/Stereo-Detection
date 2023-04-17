5ed423d5555dad9ed701b74f31f4428ddae9742b
Lecter <uxfion@gmail.com>
2023-04-17 17:14:45 +0800

    1.6m-4m

--- stereo_shot.py
+++ stereo_shot.py
@@ -6,10 +6,10 @@ counter = 1
 AUTO = True  # 自动拍照，或手动按s键拍照
 INTERVAL = 2 # 自动拍照间隔
 camera = cv2.VideoCapture(1)#也许你可能要capture两次
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)#设置分辨率
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)#
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)#设置分辨率
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)#
 utc = time.time()
-folder = "./data" # 拍照文件目录
+folder = "./data/data_3" # 拍照文件目录
 if not os.path.exists(folder):
     os.mkdir(folder)
     os.mkdir(folder + "/left")
@@ -19,8 +19,8 @@ def shot( frame):
     global counter
     leftpath = folder +"/left/"+"left_" + str(counter) + ".jpg"
     rightpath=folder + "/right/"+ "right_" + str(counter) + ".jpg"
-    leftframe=frame[0:720,0:1280]#这里是为了将合在一个窗口显示的图像分为左右摄像头
-    rightframe=frame[0:720,1280:2560]
+    leftframe=frame[0:480,0:640]#这里是为了将合在一个窗口显示的图像分为左右摄像头
+    rightframe=frame[0:480,640:1280]
     cv2.imwrite(leftpath, leftframe)
 
     cv2.imwrite(rightpath, rightframe)
